63be53985056e641afb1e08433c8acab0b85d218
Nurio Fernández <n.fernandez@efg.gg>
2024-11-14 20:02:24 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nuriofernandez/desktop-spotlight-camera-control
+
+go 1.23.2
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/joho/godotenv/autoload" // Important to keep this as the first import!
+)
+
+func main() {
+}
