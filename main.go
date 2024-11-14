4cdd03efb4c76b9df0f9044e12ce4d511d9d027d
Nurio Fernández <n.fernandez@efg.gg>
2024-11-14 21:00:30 +0100

    Implemented camera + homebridge loop

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	_ "github.com/joho/godotenv/autoload" // Important to keep this as the first import!
+
+	"time"
 )
 
 func main() {
+	for {
+		cameraStatus := checkCameraStatus()
+		_ = TurnSpotlightTo(cameraStatus)
+
+		time.Sleep(1 * time.Second)
+	}
 }
